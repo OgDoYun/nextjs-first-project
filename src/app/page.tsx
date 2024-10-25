447105a26d9aec4359412ebe30f6f2403dad4d05
옥도윤 <dhr8820@gmail.com>
2024-10-26 01:29:49 +0900

    deploy test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <h1>상기 이제 집에 좀 가자</h1>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -21,6 +22,8 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>nextjs test</li>
+          <li>nextjs test</li>
           <li>nextjs test</li>
         </ol>
 
